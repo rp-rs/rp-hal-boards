5a82f4fcb53808d18b0201e605870091c70a020c
GordonCox <GordonCox@users.noreply.github.com>
2024-07-21 07:20:12 -0700

    Formatted the code for reading
    Added comments and formmating.

--- boards/waveshare-rp2040-lcd-1-28/examples/waveshare_rp2040_lcd_demo.rs
+++ boards/waveshare-rp2040-lcd-1-28/examples/waveshare_rp2040_lcd_demo.rs
@@ -47,14 +47,16 @@ const LCD_HEIGHT: u32 = 240;
 static mut FRAME_BUFFER_1: [u8; (LCD_WIDTH * LCD_HEIGHT * 2) as usize] = [0; (LCD_WIDTH * LCD_HEIGHT * 2) as usize];
 static mut FRAME_BUFFER_2: [u8; (LCD_WIDTH * LCD_HEIGHT * 2) as usize] = [0; (LCD_WIDTH * LCD_HEIGHT * 2) as usize];
 
-
 #[entry]
 fn main() -> ! {
+    // Take ownership of peripheral instances
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
+    // Initialize watchdog
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
 
+    // Initialize clocks and PLLs
     let clocks = init_clocks_and_plls(
         XOSC_CRYSTAL_FREQ,
         pac.XOSC,
@@ -67,6 +69,7 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    // Initialize SIO
     let sio = Sio::new(pac.SIO);
     let pins = Pins::new(
         pac.IO_BANK0,
@@ -75,29 +78,22 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Set up the delay for the first core.
+    // Set up the delay for the first core
     let sys_freq = clocks.system_clock.freq().to_Hz();
     let mut delay = Delay::new(core.SYST, sys_freq);
 
     let (mut _pio, _sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
-    // https://www.waveshare.com/wiki/RP2040-LCD-0.96
-    // ST7735S LCD
+    // Initialize LCD pins
     let lcd_dc = pins.gp8.into_push_pull_output();
     let lcd_cs = pins.gp9.into_push_pull_output();
     let lcd_clk = pins.gp10.into_function::<hal::gpio::FunctionSpi>();
     let lcd_mosi = pins.gp11.into_function::<hal::gpio::FunctionSpi>();
-    let lcd_rst = pins
-        .gp12
-        .into_push_pull_output_in_state(hal::gpio::PinState::High);
-
-        let mut _lcd_bl = pins
-        .gp25
-        .into_push_pull_output_in_state(hal::gpio::PinState::Low);
+    let lcd_rst = pins.gp12.into_push_pull_output_in_state(hal::gpio::PinState::High);
+    let mut _lcd_bl = pins.gp25.into_push_pull_output_in_state(hal::gpio::PinState::Low);
     
+    // Initialize SPI
     let spi = hal::Spi::<_, _, _, 8>::new(pac.SPI1, (lcd_mosi, lcd_clk));
-
-    // Exchange the uninitialised SPI driver for an initialised one
     let spi = spi.init(
         &mut pac.RESETS,
         clocks.peripheral_clock.freq(),
@@ -105,47 +101,40 @@ fn main() -> ! {
         embedded_hal::spi::MODE_0,
     );
 
-    // LCD is a 65K IPS LCD 160x80, color order is BGR and a offset 1,26 pixel.
-    // LCD controller can correct this by settings the order bit (bit 3) in MADCTL register.
-    // Also the colours are inverted, LCD controller can also correct this by writing to INVON register with no paramters.
-    // All this is handled by the ST7735 crate.
+    // Initialize the display
     let mut display = GC9A01A::new(spi, lcd_dc, lcd_cs, lcd_rst, false, true, LCD_WIDTH, LCD_HEIGHT);
-
     display.init(&mut delay).unwrap();
-    //display.set_orientation(&Orientation::Landscape).unwrap();
 
     // Create two frame buffers for double buffering
     let mut frame_buffer_1 = unsafe { FrameBuffer::new(&mut FRAME_BUFFER_1, LCD_WIDTH, LCD_HEIGHT) };
     let mut frame_buffer_2 = unsafe { FrameBuffer::new(&mut FRAME_BUFFER_2, LCD_WIDTH, LCD_HEIGHT) };
 
-
-    //display.set_offset(1, 26);
-
+    // Define LCD dimensions
     let lcd_zero = Point::zero();
     let lcd_max_corner = Point::new((LCD_WIDTH - 1) as i32, (LCD_HEIGHT - 1) as i32);
 
+    // Define a style for the rectangle
     let style = PrimitiveStyleBuilder::new()
         .fill_color(Rgb565::BLUE)
         .build();
 
-    //Please note that you must clear the screen before you turn on the backlight or else the screen will display static.
+    // Clear the screen before turning on the backlight
     display.clear(Rgb565::BLACK);
     delay.delay_ms(1000);
-    // Set the backlight pin high
     _lcd_bl.set_high().unwrap();
     delay.delay_ms(1000);
 
+    // Draw a blue rectangle
     Rectangle::with_corners(lcd_zero, lcd_max_corner)
         .into_styled(style)
         .draw(&mut display)
         .unwrap();
+    delay.delay_ms(1000);
 
-        delay.delay_ms(1000);
-
+    // Draw a black rectangle inside the blue rectangle
     let style = PrimitiveStyleBuilder::new()
         .fill_color(Rgb565::BLACK)
         .build();
-
     Rectangle::with_corners(
         Point::new(1, 1),
         Point::new((LCD_WIDTH - 2) as i32, (LCD_HEIGHT - 2) as i32),
@@ -153,14 +142,13 @@ fn main() -> ! {
     .into_styled(style)
     .draw(&mut display)
     .unwrap();
-
     delay.delay_ms(1000);
 
+    // Draw red and green lines
     Line::new(lcd_zero, lcd_max_corner)
         .into_styled(PrimitiveStyle::with_stroke(Rgb565::RED, 1))
         .draw(&mut display)
         .unwrap();
-
     Line::new(
         Point::new(0, (LCD_HEIGHT - 1) as i32),
         Point::new((LCD_WIDTH - 1) as i32, 0),
@@ -168,78 +156,56 @@ fn main() -> ! {
     .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1))
     .draw(&mut display)
     .unwrap();
-// Load image data
-let image_data = include_bytes!("2wd-big-endian.raw");
-    // Copy image data to the frame buffer
-    //frame_buffer_1.get_mut_buffer()[..image_data.len()].copy_from_slice(image_data);
-    //frame_buffer_2.get_mut_buffer()[..image_data.len()].copy_from_slice(image_data);
 
+    // Load image data
+    let image_data = include_bytes!("2wd-big-endian.raw");
+    let raw_image: ImageRaw<Rgb565> = ImageRaw::new(image_data, LCD_WIDTH);
+    let image = Image::new(&raw_image, Point::zero());
 
-        // Draw image on the frame buffer
-        let raw_image: ImageRaw<Rgb565> = ImageRaw::new(image_data, LCD_WIDTH);
-        let image = Image::new(&raw_image, Point::zero());
-        image.draw(&mut frame_buffer_1).unwrap();
-        image.draw(&mut frame_buffer_2).unwrap();
+    // Draw the image on both frame buffers
+    image.draw(&mut frame_buffer_1).unwrap();
+    image.draw(&mut frame_buffer_2).unwrap();
+    display.show(frame_buffer_1.get_buffer()).unwrap();
+    delay.delay_ms(1000);
 
-        display.show(frame_buffer_1.get_buffer()).unwrap();
-
-        delay.delay_ms(1000);
-
-        let points = [
-            Point::new(120, 100),
-            Point::new(100, 140),
-            Point::new(140, 140),
-        ];
-
-    // Draw a filled triangle on the frame buffers
+    // Draw a filled red triangle on the first frame buffer
+    let points = [
+        Point::new(120, 100),
+        Point::new(100, 140),
+        Point::new(140, 140),
+    ];
     let style = PrimitiveStyleBuilder::new().fill_color(Rgb565::RED).build();
     Triangle::new(points[0], points[1], points[2])
         .into_styled(style)
         .draw(&mut frame_buffer_1)
         .unwrap();
-    /* 
-    Triangle::new(points[0], points[1], points[2])
-        .into_styled(style)
-        .draw(&mut frame_buffer_2)
-        .unwrap();
-    */
-    // Show the initial buffer
     display.show(frame_buffer_1.get_buffer()).unwrap();
     delay.delay_ms(1000);
 
-    // Draw the image to the display
-    //display.draw_image(image_data).unwrap();
-
-    // Draw a filled triangle on the frame buffers
+    // Draw a filled green triangle on the second frame buffer
     let style = PrimitiveStyleBuilder::new().fill_color(Rgb565::GREEN).build();
     Triangle::new(points[0], points[1], points[2])
         .into_styled(style)
         .draw(&mut frame_buffer_2)
         .unwrap();
-
-        display.show(frame_buffer_2.get_buffer()).unwrap();
+    display.show(frame_buffer_2.get_buffer()).unwrap();
     delay.delay_ms(1000);
     
-
-    //Reset the frame buffers.
+    // Reset the frame buffers
     image.draw(&mut frame_buffer_1).unwrap();
     image.draw(&mut frame_buffer_2).unwrap();
 
-        // Calculate the center of the image
-        let arrow_rotate_point_x = 240 / 2;
-        let arrow_rotate_point_y = (240 / 10) * 8;
-    create_arrow_image_5(&mut frame_buffer_1, 45, arrow_rotate_point_x,arrow_rotate_point_y );
-    create_arrow_image_5(&mut frame_buffer_2, 46, arrow_rotate_point_x,arrow_rotate_point_y );
-    //frame_buffer_2.clear(Rgb565::BLACK);
-    //image.draw(&mut frame_buffer_2).unwrap();
-    //frame_buffer_2.get_mut_buffer()[..image_data.len()].copy_from_slice(image_data);
-    
+    // Calculate the center of the image
+    let arrow_rotate_point_x = 240 / 2;
+    let arrow_rotate_point_y = (240 / 10) * 8;
+    create_arrow_image_5(&mut frame_buffer_1, 45, arrow_rotate_point_x, arrow_rotate_point_y);
+    create_arrow_image_5(&mut frame_buffer_2, 46, arrow_rotate_point_x, arrow_rotate_point_y);
+
     // Infinite colour wheel loop
     let mut l: i32 = 0;
     let mut use_first_buffer = true;
     let mut c = Rgb565::RED;
     loop {
-        // Alternate between buffers
         // Alternate between buffers
         let current_buffer = if use_first_buffer {
             frame_buffer_1.get_buffer()
@@ -247,12 +213,11 @@ let image_data = include_bytes!("2wd-big-endian.raw");
             frame_buffer_2.get_buffer()
         };
 
-
-                // Clear the current buffer
-                //current_buffer.clear(Rgb565::BLACK);
-        // Call the show function with the buffer to display the image and arrow
+        // Display the current buffer
         display.show(current_buffer).unwrap();
         //delay of 16 for 60Hz or 60 Frames a second.
+        //delay.delay_ms(1000);
+        //delay of 1 second for demo.
         delay.delay_ms(1000);
         /* 
         Line::new(Point::new(0, l), Point::new((LCD_WIDTH - 1) as i32, l))
@@ -273,11 +238,10 @@ let image_data = include_bytes!("2wd-big-endian.raw");
         // Toggle the buffer
         use_first_buffer = !use_first_buffer;
     }
-
 }
 
 fn create_arrow_image_5(
-    framebuffer: &mut FrameBuffer<>,
+    framebuffer: &mut FrameBuffer,
     angle: i32,
     compass_center_x: i32,
     compass_center_y: i32,
@@ -330,7 +294,7 @@ fn create_arrow_image_5(
     ];
 
     let red = Rgb565::new(255, 0, 0);
-    let red_9 = Rgb565::new(174, 31, 31);
+    let red_9 = Rgb565::new(19,1,1);
 
     let style_red = PrimitiveStyleBuilder::new().fill_color(red).build();
     let style_red_9 = PrimitiveStyleBuilder::new().fill_color(red_9).build();
@@ -340,9 +304,8 @@ fn create_arrow_image_5(
     draw_polygon(framebuffer, &right_points[0..4], style_red_9);
 }
 
-
 fn draw_polygon(
-    framebuffer: &mut FrameBuffer<>,
+    framebuffer: &mut FrameBuffer,
     points: &[Point],
     style: PrimitiveStyle<Rgb565>,
 ) {
@@ -350,9 +313,8 @@ fn draw_polygon(
         return; // Not enough points to form a polygon
     }
 
-    // We will use a fan triangulation from the first point
+    // Use fan triangulation from the first point
     let first_point = points[0];
-
     for i in 1..points.len() - 1 {
         let triangle = Triangle::new(first_point, points[i], points[i + 1])
             .into_styled(style);
@@ -360,7 +322,7 @@ fn draw_polygon(
     }
 }
 
-// Helper function to calculate the coordinates based on the angle and radius
+// Helper function to calculate coordinates based on angle and radius
 fn get_coordinates(center: Point, radius: i32, angle: i32) -> Point {
     let angle_rad = (angle as f32).to_radians() as f64;
     let x = center.x + (radius as f32 * cos(angle_rad) as f32) as i32;
